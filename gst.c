ae367e05a3dcc77359e1a2baf6b1a7a23e6e4e16
jj <jj>
1996-09-30 02:49:34 +0000

    Initial revision

---
+++ gst.c
@@ -0,0 +1,10 @@
+/*
+ *  $Id: gst.c,v 1.1 1996/09/30 02:49:34 jj Exp $
+ *
+ *  This is a dummy code file, which is used to generate any GST
+ *  references we want.  Just add the files to be included.
+ */
+
+#include "defs.h"
+#include <stdlib.h>
+#include <libraries/asl.h>
