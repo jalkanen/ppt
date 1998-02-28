004a57dbefec6c5c12fb96a685a68ec1892d1054
jj <jj>
1998-02-28 19:22:23 +0000

    Bessel-3

---
+++ version.h
@@ -0,0 +1,23 @@
+/*
+    PROJECT: ppt
+    MODULE : version.h
+
+    Version info for PPT.  This file is modified by IncVer.
+
+    $Id: version.h,v 1.9 1998/02/28 19:22:23 jj Exp $
+*/
+
+/*------------------------------------------------------------------*/
+
+#define VERSION         "4.43ß"
+#define VERNUM          4
+#define RELEASE         "\"Bessel-3\""
+
+#define COPYRIGHT       "(C) Janne Jalkanen 1996-1998"
+
+#define VERSDATE        __DATE__
+
+/*------------------------------------------------------------------*/
+/*                        END OF CODE                               */
+/*------------------------------------------------------------------*/
+
