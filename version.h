accbf553320fe7c9cc137e8ccdf13222dd0f0615
jj <jj>
1998-12-07 20:27:57 +0000

    Cauchy-4 release

--- version.h
+++ version.h
@@ -4,14 +4,14 @@
 
     Version info for PPT.  This file is modified by IncVer.
 
-    $Id: version.h,v 1.12 1998/10/14 20:32:36 jj Exp $
+    $Id: version.h,v 1.13 1998/12/07 20:27:57 jj Exp $
 */
 
 /*------------------------------------------------------------------*/
 
-#define VERSION         "4.60"
+#define VERSION         "4.67"
 #define VERNUM          4
-#define RELEASE         "\"Cauchy-3\""
+#define RELEASE         "\"Cauchy-4\""
 
 #define COPYRIGHT       "(C) Janne Jalkanen 1996-1998"
 
