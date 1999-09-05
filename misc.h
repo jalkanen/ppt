2e9d954d9512d54e42625cb9da7cda47d44fd302
jj <jj>
1999-09-05 17:07:09 +0000

    XLIB_FUNCS=44

--- misc.h
+++ misc.h
@@ -2,7 +2,7 @@
     PROJECT: ppt
     MODULE:  misc.h
 
-    $Id: misc.h,v 5.0 1998/12/15 23:17:33 jj Exp $
+    $Id: misc.h,v 6.0 1999/09/05 17:07:09 jj Exp $
 
     Miscallaneous defines that should NOT be put into a
     pre-compiled area.
@@ -11,7 +11,7 @@
 #ifndef MISC_H
 #define MISC_H
 
-#define XLIB_FUNCS      43
+#define XLIB_FUNCS      44
 
 #define InternalError( txt )   Debug_InternalError( txt, __FILE__, __LINE__ )
 
