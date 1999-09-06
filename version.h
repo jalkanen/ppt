9d2cf5f3c768791abfcf29bae9ab2f660272c0a1
jj <jj>
1999-09-06 21:16:41 +0000

    *** empty log message ***

--- version.h
+++ version.h
@@ -4,16 +4,16 @@
 
     Version info for PPT.  This file is modified by IncVer.
 
-    $Id: version.h,v 1.14 1998/12/20 20:00:58 jj Exp $
+    $Id: version.h,v 1.15 1999/09/06 21:16:15 jj Exp $
 */
 
 /*------------------------------------------------------------------*/
 
-#define VERSION         "5.7"
-#define VERNUM          5
-#define RELEASE         "\"Cauchy-5\""
+#define VERSION         "6.1"
+#define VERNUM          6
+#define RELEASE         "\"Descartes-1\""
 
-#define COPYRIGHT       "(C) Janne Jalkanen 1996-1998"
+#define COPYRIGHT       "(C) Janne Jalkanen 1996-1999"
 
 #define VERSDATE        __DATE__
 
